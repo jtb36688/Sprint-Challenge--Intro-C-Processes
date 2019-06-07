12301fc4f5d64bce111342fe55ad5a48caec4d0b
Jacob Bryan <jtb36688@gmail.com>
2019-06-07 12:40:51 -0400

    First pass attempt at accessing directories

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,16 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  void opened = DIR *opendir(char *path);
   // Open directory
-
+  if (!opened) {
+    fprintf(sderr, "Error Occured \n");
+    exit();
+}
+  struct dirent *readdir(opened)
   // Repeatly read and print entries
 
   // Close directory
-
+  closedir(opened)
   return 0;
 }
